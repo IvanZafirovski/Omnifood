f064804ddf2a4d421fd8f00908c8456b644acbc9
Ivan Zafirovski <ivan_zafirovski@yahoo.com>
2023-03-25 13:21:31 +0100

    finished sticky

--- JS/script.js
+++ JS/script.js
@@ -54,13 +54,17 @@ const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
     if (ent.isIntersecting === false) {
-      document.querySelector(".header").classList.add("sticky");
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
     }
   },
   {
     //in the viewport
     root: null,
     threshold: 0,
+    rootMargin: "-80px",
   }
 );
 
